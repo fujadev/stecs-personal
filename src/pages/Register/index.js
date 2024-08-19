007c5f612c70319b01acaceb0b0ec04d55d8c327
fuja <shilevelli@gmail.com>
2024-08-19 08:29:52 +0100

    vercel fixes

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,198 +1,16 @@
-// import React, { useLayoutEffect, useState } from "react";
-// import classes from "./styles.module.css";
-// import { Formik } from "formik";
-// import * as Yup from "yup";
-// import axios from "axios";
-// import { Link } from "react-router-dom";
-// import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
-// import welcomebackground from "../../assets/media/welcomebackground.png";
-// import { useLocation, useNavigate } from "react-router-dom";
-
-// // function useQuery() {
-// //   return new URLSearchParams(useLocation().search);
-// // }
-// const [searchParams] = useSearchParams();
-// 	const refCodeFromParam = searchParams.get('ref')
-
-// const validationSchema = Yup.object().shape({
-//   email: Yup.string().required().email().label("Email"),
-//   password: Yup.string()
-//     .required()
-//     .label("Password")
-//     .matches(/[A-Z]/, "Password must contain at least one uppercase character")
-//     .matches(
-//       /[!@#$%^&*()_+\-=[\]{};':"|,.<>/?]/,
-//       "Password must contain at least one special character"
-//     )
-//     .min(8),
-//   confirmPassword: Yup.string()
-//     .required()
-//     .label("Confirm Password")
-//     .oneOf([Yup.ref('password')], "Passwords must match"),
-//   referral: Yup.string().label("Referral"),
-// });
-
-
-// export default function Register() {
-//   const query = useQuery();
-//   const referralCode = query.get("code");
-//   useLayoutEffect(() => {
-//     window.scrollTo(0, 0);
-//   });
-
-//   const navigate = useNavigate();
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   return (
-//     <div className={classes.aa}>
-//       <div className={classes.wrapper}>
-//         <div className={classes.logo}>
-//           <Link to='/'>
-//             <img src="./stecs.svg" alt="logo" />
-//           </Link>
-//         </div>
-//         <img className={classes.backgroundImage} src={welcomebackground} alt="welcome" />
-//         <div className={classes.container}>
-//           <div className={classes.header}>
-//             <h1>Welcome to Stecs</h1>
-//             <p>Please Sign in to your account.</p>
-//           </div>
-//           <div className={classes.formWrapper}>
-//             <Formik
-//               initialValues={{ email: "", password: "", confirmPassword: "", referral: "" }}
-//               validationSchema={validationSchema}
-//               onSubmit={(values, { setSubmitting }) => {
-//                 console.log("Form Value", values);
-//                 axios
-//                   .post("https://strong.stecs.ng/api/sign-up", values)
-//                   .then((response) => {
-//                     navigate("/congratulations");
-//                     console.log("Sign-up successful:", response.data);
-//                   })
-//                   .catch((error) => {
-//                     console.error("Sign-up error:", error);
-//                   })
-//                   .finally(() => {
-//                     setSubmitting(false);
-//                   });
-//               }}
-//             >
-
-//               {({
-//                 handleChange,
-//                 handleSubmit,
-//                 errors,
-//                 setFieldTouched,
-//                 touched,
-//               }) => (
-//                 <form onSubmit={handleSubmit} className={classes.form}>
-//                   <div className={classes.inputField}>
-//                     <label>Email</label>
-//                     <input
-//                       onChange={handleChange("email")}
-//                       type="email"
-//                       name="email"
-//                       onBlur={() => setFieldTouched("email")}
-//                       placeholder="Enter your email"
-//                     />
-//                     {touched.email && (
-//                       <p className={classes.error}>{errors.email}</p>
-//                     )}
-//                   </div>
-//                   {/* Password */}
-//                   <div className={classes.inputField}>
-//                     <label>Password</label>
-//                     <div className={classes.passwordContainer}>
-//                       <input
-//                         onChange={handleChange("password")}
-//                         type={showPassword ? "text" : "password"}
-//                         name="password"
-//                         onBlur={() => setFieldTouched("password")}
-//                         placeholder="Enter your password"
-//                         className={classes.passwordInput}
-//                       />
-//                       <div className={classes.eyeIcon}>
-//                         {showPassword ? (
-//                           <IoEyeOutline
-//                             onClick={() => setShowPassword(false)}
-//                           />
-//                         ) : (
-//                           <IoEyeOffOutline
-//                             onClick={() => setShowPassword(true)}
-//                           />
-//                         )}
-//                       </div>
-//                     </div>
-//                     {touched.password && (
-//                       <p className={classes.error}>{errors.password}</p>
-//                     )}
-//                   </div>
-//                   {/* Confirm Password */}
-//                   <div className={classes.inputField}>
-//                     <label>Confirm Password</label>
-//                     <div className={classes.passwordContainer}>
-//                       <input
-//                         onChange={handleChange("confirmPassword")}
-//                         type={showPassword ? "text" : "password"}
-//                         name="confirmPassword"
-//                         onBlur={() => setFieldTouched("confirmPassword")}
-//                         placeholder="Confirm your password"
-//                         className={classes.passwordInput}
-//                       />
-//                       <div className={classes.eyeIcon}>
-//                         {showPassword ? (
-//                           <IoEyeOutline onClick={() => setShowPassword(false)} />
-//                         ) : (
-//                           <IoEyeOffOutline onClick={() => setShowPassword(true)} />
-//                         )}
-//                       </div>
-//                     </div>
-//                     {touched.confirmPassword && (
-//                       <p className={classes.error}>{errors.confirmPassword}</p>
-//                     )}
-//                   </div>
-//                   <div className={classes.inputField}>
-//                     <label>Referral Code</label>
-//                     <input
-//                       onChange={handleChange("referral")}
-//                       type="text"
-//                       readOnly
-//                       name="referral"
-//                       onBlur={() => setFieldTouched("referral")}
-//                       placeholder="Enter your referral code"
-//                       value={referralCode}
-//                     />
-//                     {touched.referral && (
-//                       <p className={classes.error}>{errors.referral}</p>
-//                     )}
-//                   </div>
-//                   <button className={classes.submitButton} type="submit">
-//                     Sign Up
-//                   </button>
-//                 </form>
-//               )}
-//             </Formik>
-//           </div>
-//           {/* <p className={classes.loginLinkMessage}>
-//             Already have an account? <span>Login</span>
-//           </p> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import React, { useLayoutEffect, useState } from "react";
 import classes from "./styles.module.css";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import welcomebackground from "../../assets/media/welcomebackground.png";
+import { useLocation, useNavigate } from "react-router-dom";
 
-// Validation Schema
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
   password: Yup.string()
@@ -211,15 +29,18 @@ const validationSchema = Yup.object().shape({
   referral: Yup.string().label("Referral"),
 });
 
+
 export default function Register() {
   const [searchParams] = useSearchParams();
-  const referralCode = searchParams.get("ref");
-  const navigate = useNavigate();
-  const [showPassword, setShowPassword] = useState(false);
-
+  const refCode = searchParams.get('code')
+  // const query = useQuery();
+  // const referralCode = query.get("code");
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  });
+
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className={classes.aa}>
@@ -237,7 +58,7 @@ export default function Register() {
           </div>
           <div className={classes.formWrapper}>
             <Formik
-              initialValues={{ email: "", password: "", confirmPassword: "", referral: referralCode || "" }}
+              initialValues={{ email: "", password: "", confirmPassword: "", referral: "" }}
               validationSchema={validationSchema}
               onSubmit={(values, { setSubmitting }) => {
                 console.log("Form Value", values);
@@ -255,6 +76,7 @@ export default function Register() {
                   });
               }}
             >
+
               {({
                 handleChange,
                 handleSubmit,
@@ -290,9 +112,13 @@ export default function Register() {
                       />
                       <div className={classes.eyeIcon}>
                         {showPassword ? (
-                          <IoEyeOutline onClick={() => setShowPassword(false)} />
+                          <IoEyeOutline
+                            onClick={() => setShowPassword(false)}
+                          />
                         ) : (
-                          <IoEyeOffOutline onClick={() => setShowPassword(true)} />
+                          <IoEyeOffOutline
+                            onClick={() => setShowPassword(true)}
+                          />
                         )}
                       </div>
                     </div>
@@ -333,7 +159,7 @@ export default function Register() {
                       name="referral"
                       onBlur={() => setFieldTouched("referral")}
                       placeholder="Enter your referral code"
-                      value={referralCode || ""}
+                      value={refCode}
                     />
                     {touched.referral && (
                       <p className={classes.error}>{errors.referral}</p>
@@ -346,8 +172,12 @@ export default function Register() {
               )}
             </Formik>
           </div>
+          {/* <p className={classes.loginLinkMessage}>
+            Already have an account? <span>Login</span>
+          </p> */}
         </div>
       </div>
     </div>
   );
 }
+
